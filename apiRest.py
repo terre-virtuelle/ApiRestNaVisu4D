4bbe36757f30536cc0d894002900e6204ec46970
serge <serge@serge-XPS-15-9550>
2020-12-28 11:52:25 +0100

    Server ApiRest and test python

---
+++ apiRest.py
@@ -0,0 +1,19 @@
+import requests
+import time
+import random
+
+
+i = 0
+sign = -1
+range = 0
+while i < 10:
+ lat = str(48.00 + range)
+ lon = str(-4.50 - range)
+ r = requests.put('http://localhost:3003/control?cmd=flyTo&origin=SMAUG&target=camera&latitude='+ lat +'&longitude='+lon+'&altitude=300000&heading=0.0&pitch=-80.0&roll=0.0')
+
+ sign *= sign
+ range = sign * random.randint(0, 50) 
+ time.sleep(1)
+ i = i + 0.5 
+
+
