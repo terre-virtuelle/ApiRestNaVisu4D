8c23b0767e940ffd241291e75be21889237ddb2f
serge <serge@serge-XPS-15-9550>
2020-12-30 10:31:34 +0100

    Server ApiRest and test python

--- apiRest.py
+++ apiRest.py
@@ -9,8 +9,8 @@ range = 0
 while i < 10:
  lat = str(48.00 + range)
  lon = str(-4.50 - range)
- r = requests.put('http://localhost:3003/control?cmd=flyTo&origin=SMAUG&target=camera&latitude='+ lat +'&longitude='+lon+'&altitude=300000&heading=0.0&pitch=-80.0&roll=0.0')
-
+ r = requests.put('http://93.90.200.21:3003/control?cmd=flyTo&origin=SMAUG&target=camera&latitude='+ lat +'&longitude='+lon+'&altitude=300000&heading=0.0&pitch=-80.0&roll=0.0')
+ str(r);
  sign *= sign
  range = sign * random.randint(0, 50) 
  time.sleep(1)
